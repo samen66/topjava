78e0e3d9af2f85e599c2df5e57c766b848696704
SAmen <samenuatkhan@gmail.com>
2023-03-01 12:33:30 +0600

    hw3 db init

---
+++ src/main/resources/db/populateDBMeal.sql
@@ -0,0 +1,16 @@
+DELETE FROM user_role;
+DELETE FROM users;
+DELETE FROM meals;
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+ALTER SEQUENCE global_seq_meal RESTART WITH 100000;
+
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin'),
+       ('Guest', 'guest@gmail.com', 'guest');
+
+INSERT INTO user_role (role, user_id)
+VALUES ('USER', 100000),
+       ('ADMIN', 100001);
+
+
